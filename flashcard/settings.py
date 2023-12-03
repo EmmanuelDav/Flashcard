1374a3b2d4e1be1bc4a6c2e41293d04b8e0eb82d
emmanueldav <kuradavis987@gmail.com>
2023-12-03 17:38:11 +0100

    update on last commit

--- flashcard/settings.py
+++ flashcard/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6952twt%9ma%%8#9(f)5*x-cw+ay%c%0(%c+9m_kc^o-!)a1%b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['flashcard-env.eba-8vuhqgbi.us-west-2.elasticbeanstalk.com']
 
